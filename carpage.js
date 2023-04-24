4e057a8ae3d90bda22a992ba037b9ec80d20fddc
saravana3333 <sarosaravana333@gmail.com>
2023-04-24 23:15:12 +0530

    car addidng

--- carpage.js
+++ carpage.js
@@ -48,3 +48,23 @@ carousel.addEventListener("touchmove", dragging);
 carousel.addEventListener("mouseup", dragStop);
 carousel.addEventListener("mouseleave", dragStop);
 carousel.addEventListener("touchend", dragStop);
+
+
+const arrows = document.querySelectorAll(".arrow");
+const container= document.querySelectorAll(".container");
+
+arrows.forEach((arrow, i) => {
+    const ItemNo = container[i].querySelectorAll("img").length;
+    let clickitem = 0;
+    arrow.addEventListener("click", () => {
+        clickitem++;
+        if(ItemNo - (5 + clickitem) >= 0){
+            container[i].style.transform = `translateX(${
+                container[i].computedStyleMap().get("transform")[0].x.value
+                - 455}px)`;
+        }else{
+            container[i].style.transform = "translateX(0)";
+            clickitem = 0;
+        }
+    });
+});
